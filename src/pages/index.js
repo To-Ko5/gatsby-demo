b9561eca1e78d741bfc6770b34cfa93db02f1983
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-11-03 21:32:39 +0900

    create project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function home() {
+  return (
+    <div>
+      <header className="header">
+        <h1 className="header__title">タイトル</h1>
+      </header>
+      <section className="hero">
+        <img src="/images/pic1.jpg" alt="hero" className="hero__img"></img>
+      </section>
+    </div>
+  )
 }
