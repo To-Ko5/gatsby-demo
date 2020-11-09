680288c8655895bde2ed0af7c9b9766ef5ff6d3d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-11-09 09:40:24 +0900

    about page作成

---
+++ src/pages/about.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+import Layouts from '../components/Layouts'
+
+export default function about({ data }) {
+  return (
+    <Layouts>
+      <p>about</p>
+      <figure>
+        <Img fluid={data.pic4.childImageSharp.fluid}></Img>
+      </figure>
+    </Layouts>
+  )
+}
+
+export const query = graphql`
+  query MyQuery2 {
+    pic4: file(relativePath: { eq: "pic4.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1980) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
