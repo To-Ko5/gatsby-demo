55111020bc9940f55bf2c3c7cc3775150d632474
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2020-11-09 10:21:10 +0900

    リンクを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const localStyle = require('../style/header.module.scss')
 
 export default function Header() {
   return (
     <header className={`${localStyle.header}`}>
-      <h1 className={`${localStyle.header__title}`}>タイトル</h1>
+      <h1 className={`${localStyle.header__title}`}>
+        <Link to={'/'}>タイトル</Link>
+      </h1>
+      
+      <nav>
+        <ul className={`${localStyle.gnav}`}>
+          <li className={`${localStyle.gnav__list}`}>
+            <Link to={`/about`} className={`${localStyle.gnav__link}`}>
+              about
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
